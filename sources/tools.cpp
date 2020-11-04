102f720d29e29fa789854672a7ad9a352c6f3ae8
Alexis Delhaie <alexis.delhaie@yahoo.com>
2020-11-04 20:11:16 +0100

    fix 17h010

--- sources/tools.cpp
+++ sources/tools.cpp
@@ -2,13 +2,12 @@
 
 QString Tools::double_to_string_time(double value) {
     const int h = floor(value);
-    value -= h;
-    value = value * 60;
+    value = round((value - h) * 60);
     if (value == 0.0) {
         return QString("%1h").arg(QString::number(h));
     } else if (value < 10.0) {
-        return QString("%1h0%2").arg(QString::number(h), QString::number(round(value)));
+        return QString("%1h0%2").arg(QString::number(h), QString::number(value));
     }
 
-    return QString("%1h%2").arg(QString::number(h), QString::number(round(value)));
+    return QString("%1h%2").arg(QString::number(h), QString::number(value));
 }
